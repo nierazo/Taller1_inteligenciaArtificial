aa6902c5a071c44a0b55f92898bae5f4883ce436
Sesguerra1 <santiago.esguerra2006@outlook.com>
2026-08-28 16:47:51 -0500

    heuristicas de A*

--- EstacionEspacial/algorithms/heuristics.py
+++ EstacionEspacial/algorithms/heuristics.py
@@ -21,8 +21,17 @@ def manhattanHeuristic(state, problem):
     - the nearest pending T if the robot has the kit and systems remain.
     - C if all systems have been repaired.
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    position, hasKit, pendingSystems = state
+    
+    if not hasKit:
+        problem.kitPosition
+    elif len(pendingSystems) > 0:
+        target min( pendingSystems,
+            key=lambda t: abs(position[0] - t[0]) + abs(position[1] - t[1]),)
+    else:
+        target = problem.controlPosition
+
+    return abs(position[0] - target[0]) + abs(position[1] - target[1])
 
 
 def euclideanHeuristic(state, problem):
@@ -35,8 +44,19 @@ def euclideanHeuristic(state, problem):
     - the nearest pending T if the robot has the kit and systems remain.
     - C if all systems have been repaired.
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    position, hasKit, pendingSystems = state
+
+    if not hasKit:
+        target = problem.kitPosition
+    elif len(pendingSystems) > 0:
+        target = min(
+            pendingSystems,
+            key=lambda t: ((position[0] - t[0]) ** 2 + (position[1] - t[1]) ** 2) ** 0.5,
+        )
+    else:
+        target = problem.controlPosition
+
+    return ((position[0] - target[0]) ** 2 + (position[1] - target[1]) ** 2) ** 0.5
 
 
 def systemRepairHeuristic(
@@ -56,5 +76,15 @@ def systemRepairHeuristic(
     - Consider the kit, pending systems, and the final return to control center
     - Balance heuristic strength vs. computation time (do experiments!)
     """
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+    
+    position, hasKit, pendingSystems = state
+
+    required = list(pendingSystems)
+    if not hasKit:
+        required.append(problem.kitPosition)
+    required.append(problem.controlPosition)
+
+    return max(
+        abs(position[0] - p[0]) + abs(position[1] - p[1])
+        for p in required
+    )
